718ea576008e10439441b151cf815adfc6a8884f
Jeong seok tae <77574555+nineking424@users.noreply.github.com>
2022-10-13 22:10:14 +0900

    Example for P110

---
+++ main2.py
@@ -0,0 +1,31 @@
+
+from p110 import P110
+import logging
+import argparse
+
+parser = argparse.ArgumentParser(description="Change plug state.")
+parser.add_argument('tplink_email', metavar='TPLINK_EMAIL', type=str, help="Your TPLink account email")
+parser.add_argument('tplink_password', metavar='TPLINK_PASS', type=str, help="Your TPLink account password")
+parser.add_argument('address', metavar='ADDR', type=str, help="Address of your plug (ex. 192.168.2.22)")
+parser.add_argument('new_state', metavar='STATE', type=int, help="New state of the plug (on=1 off=0) ")
+
+
+args = parser.parse_args()
+
+
+logger = logging.getLogger('root')
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+logger.setLevel(logging.INFO)
+
+logger.info(f"Will change state of plug at '{args.address}' to '{args.new_state}'")
+
+my_plug = P110(args.address)
+my_plug.handshake()
+my_plug.login_request(args.tplink_email, args.tplink_password)
+state = my_plug.get_state()
+logger.info(f"Returned result: {state}")
+
+energy = my_plug.get_energy()
+logger.info(f"Returned result: {energy}")
+
